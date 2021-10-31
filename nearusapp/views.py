36208f072678626236630ca1cabec33ecdd984e4
reveriess <sayid159@gmail.com>
2021-10-31 22:29:57 +0700

    Refactor gmaps-related functions into GMaps class

--- nearusapp/views.py
+++ nearusapp/views.py
@@ -3,17 +3,12 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from staticmaps import staticmaps_func
 from script import (
-    get_places,
-    unpack_places,
-    unpack_target_places,
+    gmaps,
     Place,
     centroid,
     haversine_distance,
     distance_based_decision,
-    get_places_target,
 )
-
-# Create your views here.
 
 from .forms import UserLocation
 
@@ -36,22 +31,23 @@ def get_location(request):
             address_2 = form.cleaned_data["user_place_2"]
             address_3 = form.cleaned_data["user_place_3"]
             address_4 = form.cleaned_data["target_place"]
+
             user_addresses = [address_1, address_2, address_3]
             user_places = [
-                Place(unpack_places(get_places(ad_users)))
-                for ad_users in user_addresses
+                Place(gmaps.get_place(ad_users)) for ad_users in user_addresses
             ]
             user_latlong = [i.get_latlong() for i in user_places]
+
             centroid_users = centroid(user_places)
             target_places = [
                 Place(ad_target)
-                for ad_target in unpack_target_places(
-                    get_places_target(address_4, centroid_users)
-                )
+                for ad_target in gmaps.get_places_target(address_4, centroid_users)
             ]
+
             centroid_users, result_short, result_place = distance_based_decision(
                 5, target_places, user_places
             )
+
             target_latlong = [i.get_latlong() for i in result_place]
             staticimg_url = staticmaps_func(
                 staticimg_url, user_latlong, target_latlong, centroid_users
