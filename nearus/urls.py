98f7677394b044558ea98d5515a8be54336a7438
sage <laymonage@gmail.com>
2021-11-06 23:01:17 +0700

    Add media file config

--- nearus/urls.py
+++ nearus/urls.py
@@ -13,11 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from nearusapp import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',views.get_location),
-]
+    path("admin/", admin.site.urls),
+    path("", views.get_location),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
