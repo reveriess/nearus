2fa8516c7161374cdb810ae017dc7453f7f68613
sage <laymonage@gmail.com>
2021-11-06 23:01:50 +0700

    Refactor `staticmaps_func` to use `requests` in handling params

--- staticmaps.py
+++ staticmaps.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from urllib.parse import urlencode
 import base64
 import os
@@ -7,44 +8,23 @@ import requests
 API_KEY = os.environ.get("API_KEY")
 
 
+def latlong_to_str(latlong):
+    return ",".join(str(v) for v in latlong)
+
+
 # user_latlong=list of lists
-def staticmaps_func(user_latlong, target_latlong, centroid_users):
+def staticmaps_func(user_latlongs, target_latlongs, centroid_users):
     api_url = "https://maps.googleapis.com/maps/api/staticmap?"
-    markers_str = ""
-    num_users = len(user_latlong)
-    num_targets = len(target_latlong)
 
-    # Users' marker
-    markers_str = markers_str + "markers=color:blue%7Clabel:U%7C"
-    for i in range(num_users):
-        counter = 0
-        for j in user_latlong[i]:  # user_latlong is a list of list
-            counter = counter + 1
-            markers_str = markers_str + "".join(str(j))
-            if counter == 1:  # comma is inserted between lat and long
-                markers_str = markers_str + ","
-        if i != num_users - 1:
-            markers_str = markers_str + "%7C"  #%7C is inserted between each latlongs
-    markers_str = markers_str + "&markers=color:red%7Clabel:T%7C"
+    user_markers = ["color:blue", "label:U"] + map(latlong_to_str, user_latlongs)
+    target_markers = ["color:red", "label:T"] + map(latlong_to_str, target_latlongs)
+    centroid_markers = ["color:yellow", "label:C"] + map(latlong_to_str, centroid_users)
 
-    # Targets' marker
-    for i in range(num_targets):
-        counter = 0
-        for j in target_latlong[i]:
-            counter = counter + 1
-            markers_str = markers_str + "".join(str(j))
-            if counter == 1:
-                markers_str = markers_str + ","
-        if i != num_targets - 1:
-            markers_str = markers_str + "%7C"
-
-    # Centroid's marker
-    markers_str = (
-        markers_str
-        + "&markers=color:yellow%7Clabel:C%7C"
-        + ",".join([str(c) for c in centroid_users])
-    )
-    print(markers_str)
+    markers = [
+        "|".join(user_markers),
+        "|".join(target_markers),
+        "|".join(centroid_markers),
+    ]
 
     params = {
         "center": str(centroid_users),
@@ -54,19 +34,31 @@ def staticmaps_func(user_latlong, target_latlong, centroid_users):
         "maptype": "roadmap",
         "style": "feature:poi|visibility:off",
         "key": API_KEY,
+        "markers": markers,
     }
-    strparams = urlencode(params)
-    final_url = f"{api_url}{markers_str}&{strparams}"
 
-    base64data = download_as_base64(final_url)
-    data_uri = base64_to_data_uri(base64data)
-    return data_uri
+    filename = download_to_media(api_url, params)
+    data_url = get_media_url(filename)
+    return data_url
 
 
-def download_as_base64(url):
-    response = requests.get(url)
+def download_to_media(url, params):
+    response = requests.get(url, params=params)
+    now = datetime.now().isoformat()
+    filename = f"{now}.png"
+    with open(f"media/{filename}", "wb") as f:
+        f.write(response.content)
+    return filename
+
+
+def get_media_url(filename):
+    return f"/media/{filename}"
+
+
+def download_as_base64(url, params):
+    response = requests.get(url, params=params)
     return base64.b64encode(response.content)
 
 
 def base64_to_data_uri(base64_str):
-    return "data:image/png;base64," + base64_str.decode("utf-8")
+    return "data:image/png;base64," + base64_str.decode()
